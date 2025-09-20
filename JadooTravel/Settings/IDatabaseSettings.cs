0ce500e64becf85dc717413be12eb9c00e64b8df
EMRE OKAN BAŞKAYA <emreokanbaskaya@gmail.com>
2025-09-20 19:23:14 +0300

    -Add ViewComponents CategoryDtos DestinationDtos

---
+++ JadooTravel/Settings/IDatabaseSettings.cs
@@ -0,0 +1,12 @@
+﻿namespace JadooTravel.Settings
+{
+    public interface IDatabaseSettings
+    {
+        public string ConnectionString { get; set; }
+        public string Database { get; set; }
+        public string CategoryCollectionName { get; set; }
+        public string DestinationCollectionName { get; set; }
+        public string FeatureCollectionName { get; set; }
+        public string TripPlanCollectionName { get; set; }
+    }
+}
