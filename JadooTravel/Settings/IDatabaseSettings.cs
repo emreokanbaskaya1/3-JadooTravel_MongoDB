d6efb2da2ea707daff1f137da8ac354b6c8f6a8c
EMRE OKAN BAŞKAYA <emreokanbaskaya@gmail.com>
2025-09-25 22:48:18 +0300

    Add MongoDB and AutoMapper support with CategoryService
    Added `AutoMapper` and `MongoDB.Driver` packages to the project for object mapping and database operations. Updated `IDatabaseSettings` to rename `Database` to `DatabaseName` for clarity. Introduced `CategoryService` for CRUD operations on categories, leveraging MongoDB and AutoMapper. Added `ICategoryService` and `IDestinationService` interfaces to define contracts for category and destination services, respectively.

--- JadooTravel/Settings/IDatabaseSettings.cs
+++ JadooTravel/Settings/IDatabaseSettings.cs
@@ -3,7 +3,7 @@
     public interface IDatabaseSettings
     {
         public string ConnectionString { get; set; }
-        public string Database { get; set; }
+        public string DatabaseName { get; set; }
         public string CategoryCollectionName { get; set; }
         public string DestinationCollectionName { get; set; }
         public string FeatureCollectionName { get; set; }
